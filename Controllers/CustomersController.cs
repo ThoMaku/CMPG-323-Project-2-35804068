21dd60b2e5b5d9de73fe8ccbd0036f67efb4ea6a
ThobileMaku <35804068@student.g.nwu.ac.za>
2023-08-30 04:09:29 +0200

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using CMPG323_Project2.Models;
+using System.Collections;
+
+namespace CMPG323_Project2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly CMPG323Project2Context _context;
+
+        public CustomersController(CMPG323Project2Context context)
+        {
+            _context = context;
+        }
+
+        //GET: Customers
+        [HttpGet]
+        public async Task<ActionResult<Customer>> Index()
+        {
+            if (_context.Customers == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                var customers = await _context.Customers.ToListAsync();
+                return View(customers);
+            }
+            
+        }
+
+        // GET: Customers/Details/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> Details(short? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Index), new {customerId = customer.CustomerId}, customer);
+        }
+
+        /*// GET: Customers/Create
+        [HttpGet("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'CMPG323Project2DBContext.Stores'  is null.");
+            }
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer    );
+        }
+
+
+        // GET: Customers/Edit/5
+        public async Task<IActionResult> Edit(short? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: Customers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(short id, [Bind("CustomerId,CustomerTitle,CustomerName,CustomerSurname,CellPhone")] Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customer);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerExists(customer.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(customer);
+        }
+
+        // GET: Customers/Delete/5
+        public async Task<IActionResult> Delete(short? id)
+        {
+            if (id == null || _context.Customers == null)
+            {
+                return NotFound();
+            }
+
+            var customer = await _context.Customers
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return View(customer);
+        }
+
+        // POST: Customers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(short id)
+        {
+            if (_context.Customers == null)
+            {
+                return Problem("Entity set 'CMPG323Project2Context.Customers'  is null.");
+            }
+            var customer = await _context.Customers.FindAsync(id);
+            if (customer != null)
+            {
+                _context.Customers.Remove(customer);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerExists(short id)
+        {
+          return (_context.Customers?.Any(e => e.CustomerId == id)).GetValueOrDefault();
+        }*/
+    }
+}
